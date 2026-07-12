f776ff99dd581e3cea97da7dfc387c0f5cee1ef5
LittleOctopus233 <19852913480@163.com>
2026-07-12 15:14:15 +0800

    改变显示格式
    修复部分问题，改变显示格式

--- GodotPlanner/main.cpp
+++ GodotPlanner/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 #include <conio.h>
 #include <windows.h>
@@ -9,40 +9,68 @@
 #pragma comment(linker, "/SUBSYSTEM:WINDOWS")
 #pragma comment(linker, "/ENTRY:wmainCRTStartup")
 
-void ShowMenu() {
+void ShowBasicInfo() {
+	WriteLineW(L"================================");
+	WriteLineW(L"Godot统筹 v0.3-console");
 	WriteLineW(L"");
+	WriteLineW(L"made by : OctopusZhang");
+	WriteLineW(L"made date : 2026/7/12");
+	WriteLineW(L"");
+	WriteLineW(L"纳西妲世界第一可爱");
 	WriteLineW(L"================================");
-	WriteLineW(L"请选择操作:");
-	WriteLineW(L"1. 打开引擎");
-	WriteLineW(L"2. 打开项目");
-	WriteLineW(L"3. 退出");
-	WriteLineW(L"================================");
-	WriteW(L"输入选项(1-3): ");
+	WriteLineW(L"");
 }
 
-void SelectEngine(const std::vector<GodotEngine>& engines) {
+void ShowMenu() {
+	WriteLineW(L"================================");
+	WriteLineW(L"    Godot统筹 v0.3-console");
+	WriteLineW(L"================================");
+	WriteLineW(L"(a) 打开引擎      (b) 打开项目");
+	WriteLineW(L"================================");
+}
+
+void ShowDownMenu() {
+	WriteLineW(L"================================");
+	WriteLineW(L"(q) 上一页        (e) 下一页  ");
+	WriteLineW(L"================================");
+	WriteLineW(L"(c) 退出程序      (d) 关于项目");
+	WriteLineW(L"================================");
+}
+
+void ShowSelectEngineInfo(const std::vector<GodotEngine>& engines, int page = 1) {
 	if(engines.empty()) {
 		MessageBoxW(NULL, L"未找到任何Godot引擎！", L"错误", MB_OK | MB_ICONERROR);
 		return;
 	}
 
-	WriteLineW(L"");
-	WriteLineW(L"可用引擎列表:");
-	for(size_t i = 0; i < engines.size(); ++i) {
-		WriteLineW(std::to_wstring(i + 1) + L". " + engines[i].GetDisplayName());
+	WriteLineW(L"请选择引擎:");
+	for(size_t i = (page - 1) * 10; i < engines.size() && i < page * 10; ++i) {
+		WriteLineW(L"(" + std::to_wstring(i + 1 - (page - 1) * 10) + L"). " + engines[i].GetDisplayName());
+	}
+}
+
+void ShowSelectProjectInfo(const std::vector<GodotProject>& projects, int page = 1) {
+	if(projects.empty()) {
+		MessageBoxW(NULL, L"未找到任何Godot项目！", L"错误", MB_OK | MB_ICONERROR);
+		return;
 	}
 
-	WriteW(L"输入引擎序号: ");
-	int choice = _getch() - '0';
-	WriteLineW(std::to_wstring(choice));
+	WriteLineW(L"请选择项目:");
+	for(size_t i = (page - 1) * 10; i < projects.size() && i < page * 10; ++i) {
+		WriteLineW(L"(" + std::to_wstring(i + 1 - (page - 1) * 10) + L"). " + projects[i].GetProjectName() + L" [Godot " + projects[i].GetVersion() + L"]");
+	}
+}
 
-	if(choice < 1 || static_cast<size_t>(choice) > engines.size()) {
+void SelectEngine(const std::vector<GodotEngine>& engines, int choice, int page = 1) {
+	if (choice < 1 || choice > 10 || choice > engines.size() - (page - 1) * 10) {
 		MessageBoxW(NULL, L"无效选项！", L"错误", MB_OK | MB_ICONERROR);
 		return;
 	}
 
-	const GodotEngine& engine = engines[choice - 1];
-	if(engine.LaunchProjectManager()) {
+	WriteLineW(L"开始启动引擎: " + engines[(page - 1) * 10 + choice - 1].GetDisplayName());
+
+	const GodotEngine& engine = engines[(page - 1) * 10 + choice - 1];
+	if (engine.LaunchProjectManager()) {
 		exit(0);
 	}
 	else {
@@ -50,30 +78,20 @@ void SelectEngine(const std::vector<GodotEngine>& engines) {
 	}
 }
 
-void SelectProject(const std::vector<GodotEngine>& engines, const std::vector<GodotProject>& projects) {
-	if(projects.empty()) {
-		MessageBoxW(NULL, L"未找到任何Godot项目！", L"错误", MB_OK | MB_ICONERROR);
-		return;
-	}
-
-	WriteLineW(L"");
-	WriteLineW(L"可用项目列表:");
-	for(size_t i = 0; i < projects.size(); ++i) {
-		WriteLineW(std::to_wstring(i + 1) + L". " + projects[i].GetProjectName() + L" [Godot " + projects[i].GetVersion() + L"]");
-	}
-
-	WriteW(L"输入项目序号: ");
-	int choice = _getch() - '0';
-	WriteLineW(std::to_wstring(choice));
-
-	if(choice < 1 || static_cast<size_t>(choice) > projects.size()) {
+void SelectProject(const std::vector<GodotEngine>& engines, const std::vector<GodotProject>& projects, int choice, int page = 1) {
+	if(choice < 1 || choice > 10 || choice > projects.size() - (page - 1) * 10) {
 		MessageBoxW(NULL, L"无效选项！", L"错误", MB_OK | MB_ICONERROR);
 		return;
 	}
 
-	const GodotProject& project = projects[choice - 1];
+	WriteLineW(L"开始启动项目: " + projects[(page - 1) * 10 + choice - 1].GetProjectName());
+
+	const GodotProject& project = projects[(page - 1) * 10 + choice - 1];
 	if(OpenProject(engines, project.GetProjectPath())) {
 		exit(0);
+	}
+	else {
+		MessageBoxW(NULL, L"项目启动失败！", L"错误", MB_OK | MB_ICONERROR);
 	}
 }
 
@@ -96,7 +114,6 @@ int wmain(int argc, wchar_t* argv[]) {
 	}
 	else {
 		ShowConsole();
-		ShowBasicInfo();
 
 		if(!LoadEngines(engines)) {
 			return 1;
@@ -106,23 +123,61 @@ int wmain(int argc, wchar_t* argv[]) {
 			return 1;
 		}
 
+		int mode = 1;// 1: engine mode, 2: project mode
+		int page = 1;
+
 		while(true) {
+			system("cls");
 			ShowMenu();
-			int choice = _getch() - '0';
-			WriteLineW(std::to_wstring(choice));
+
+			if (mode == 1) {
+				ShowSelectEngineInfo(engines, page);
+			}
+			else if (mode == 2) {
+				ShowSelectProjectInfo(projects, page);
+			}
+
+			ShowDownMenu();
+
+			int choice = _getch();
 
 			switch(choice) {
-			case 1:
-				SelectEngine(engines);
+			case 'a':
+				mode = 1;
+				page = 1;
 				break;
-			case 2:
-				SelectProject(engines, projects);
+			case 'b':
+				mode = 2;
+				page = 1;
 				break;
-			case 3:
-				WriteLineW(L"退出程序...");
+			case 'c':
 				return 0;
+			case 'd':
+				system("cls");
+				ShowBasicInfo();
+				WriteLineW(L"按任意键返回菜单...");
+				_getch();
+				break;
+			case 'q':
+				if (page > 1) {
+					page--;
+				}
+				break;
+			case 'e':
+				if (mode == 1 && page * 10 < engines.size()) {
+					page++;
+				}
+				else if(mode == 2 && page * 10 < projects.size()) {
+					page++;
+				}
+				break;
 			default:
-				MessageBoxW(NULL, L"无效选项，请重新输入！", L"错误", MB_OK | MB_ICONERROR);
+				if (mode == 1) {
+					SelectEngine(engines, choice - '0', page);
+				}
+				else if (mode == 2) {
+					SelectProject(engines, projects, choice - '0', page);
+				}
 				break;
 			}
 		}
